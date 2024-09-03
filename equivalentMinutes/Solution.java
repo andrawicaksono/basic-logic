63854985a9a22a2bc5ced83935cf69ac0b70db13
andrawicaksono <andrawicaksono14@gmail.com>
2024-09-03 14:35:40 +0700

    add equivalent minutes

---
+++ equivalentMinutes/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+  static int equivalentMinutes(int minutes, int seconds) {
+    int number = 0;
+
+    double planetSeconds = 1.0 * minutes * seconds;
+    number = (int) Math.ceil(planetSeconds / 60);
+
+    return number;
+  }
+
+  public static void main(String[] args) {
+    int minutes;
+    int seconds;
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      minutes = scanner.nextInt();
+      seconds = scanner.nextInt();
+    }
+
+    int equivalentMinutes = equivalentMinutes(minutes, seconds);
+
+    System.out.println(equivalentMinutes);
+  }
+}
